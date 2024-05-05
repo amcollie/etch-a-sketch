d3e1e67618223f5077acacbe6cee25deb12a7c1c
Alexandros Collie <amcollie@gmail.com>
2024-05-05 12:58:51 -0400

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.grid-container')
+const gridSizeButton = document.querySelector('#grid-size')
+gridSizeButton.addEventListener('click', () => {
+    
+    const gridSize = parseInt(prompt('Please enter grid size'))
+    
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        for (let j = 0; j < gridSize; j++) {
+            const column = document.createElement('div')
+            column.classList.add('column')
+            column.addEventListener('mouseleave', (e) => {
+                e.target.style.backgroundColor = '#efff32'
+            })
+            row.appendChild(column)
+        }
+        
+        container.appendChild(row)
+    }
+})
